8419a3d2c5c96b9ef82db44fd6afa04c6f40f44a
Phillip Oldham <phillip.oldham@gmail.com>
2019-06-16 22:07:24 +0200

    Publish to hex.pm
    - add licence
    - add changelog
    - update mix.exs settings for publishing

--- mix.exs
+++ mix.exs
@@ -6,23 +6,34 @@ defmodule LoggerEtsBackend.MixProject do
       app: :logger_ets_backend,
       version: "0.0.1",
       elixir: "~> 1.8",
-      # start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp description() do
+    "A simple `Logger` backend which writes logs to an ETS table."
+  end
+
+  defp package() do
+    [
+      name: "logger_ets_backend",
+      files: ~w(lib .formatter.exs mix.exs README.md LICENSE CHANGELOG.md),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/OldhamMade/logger_ets_backend"}
     ]
   end
 end
