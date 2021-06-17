3495a9bf9a5e5d32720ad393c485399818309440
OldhamMade <OldhamMade@users.noreply.github.com>
2021-06-17 15:47:58 +0200

    Replace Travis with Github Workflows
    [changelog]
    added: coverage check
    changed: min version now 1.6
    changed: replace Travis with Github CI workflow
    changed: add Github release workflow
    changed: updated CI badge in README
    changed: moduledoc for main module to pull from README
    changed: MIT licence replaced with Apache 2.0 licence
    fixed: formatting

--- test/logger_ets_backend_test.exs
+++ test/logger_ets_backend_test.exs
@@ -11,42 +11,44 @@ defmodule LoggerEtsBackendTest do
 
   setup_all do
     :ets.new(@name, [:ordered_set, :public, :named_table])
-    config level: :debug
+    config(level: :debug)
   end
 
   describe "error handling during setup" do
     test "does not crash if table does not exist" do
-      config table: nil
+      config(table: nil)
 
-      Logger.debug "foo"
+      Logger.debug("foo")
       assert {:error, :already_present} = Logger.add_backend(@backend)
     end
   end
 
   describe "basic " do
     setup do
-      config [
+      config(
         table: @name,
         level: :debug,
         metadata: [],
         metadata_filter: nil
-      ]
+      )
 
-      on_exit fn ->
+      on_exit(fn ->
         :ets.delete_all_objects(@name)
-      end
+      end)
     end
 
     test "add log entry" do
       Logger.info("simple message")
-      Logger.flush()  # to ensure messages are written to ets
+      # to ensure messages are written to ets
+      Logger.flush()
 
       assert ets_size() == 1
     end
 
     test "can log utf8 chars" do
       Logger.info("ß\uFFaa\u0222")
-      Logger.flush()  # to ensure messages are written to ets
+      # to ensure messages are written to ets
+      Logger.flush()
 
       {_ts, _level, msg, _md} = log()
       assert ets_size() == 1
@@ -54,42 +56,51 @@ defmodule LoggerEtsBackendTest do
     end
 
     test "can configure level" do
-      config level: :info
+      config(level: :info)
 
       Logger.debug("hello")
       assert ets_size() == 0
     end
 
     test "can configure metadata_filter to capture" do
-      config metadata_filter: [capture: true]
+      config(metadata_filter: [capture: true])
 
       Logger.debug("should be skipped", capture: false)
-      Logger.flush()  # to ensure messages are written to ets
+      # to ensure messages are written to ets
+      Logger.flush()
       assert ets_size() == 0
 
       Logger.debug("should be logged", capture: true)
-      Logger.flush()  # to ensure messages are written to ets
+      # to ensure messages are written to ets
+      Logger.flush()
       assert ets_size() == 1
     end
 
     test "metadata_matches?" do
-      assert metadata_matches?([a: 1], [a: 1]) == true # exact match
-      assert metadata_matches?([b: 1], [a: 1]) == false # total mismatch
-      assert metadata_matches?([b: 1], nil) == true # default to allow
-      assert metadata_matches?([b: 1, a: 1], [a: 1]) == true # metadata is superset of filter
-      assert metadata_matches?([c: 1, b: 1, a: 1], [b: 1, a: 1]) == true # multiple filter keys subset of metadata
-      assert metadata_matches?([a: 1], [b: 1, a: 1]) == false # multiple filter keys superset of metadata
+      # exact match
+      assert metadata_matches?([a: 1], a: 1) == true
+      # total mismatch
+      assert metadata_matches?([b: 1], a: 1) == false
+      # default to allow
+      assert metadata_matches?([b: 1], nil) == true
+      # metadata is superset of filter
+      assert metadata_matches?([b: 1, a: 1], a: 1) == true
+      # multiple filter keys subset of metadata
+      assert metadata_matches?([c: 1, b: 1, a: 1], b: 1, a: 1) == true
+      # multiple filter keys superset of metadata
+      assert metadata_matches?([a: 1], b: 1, a: 1) == false
     end
 
     test "can configure metadata" do
-      config metadata: [:user_id, :auth]
+      config(metadata: [:user_id, :auth])
 
       Logger.metadata(auth: true)
       Logger.metadata(user_id: 11)
       Logger.metadata(user_id: 13)
 
       Logger.debug("hello")
-      Logger.flush()  # to ensure messages are written to ets
+      # to ensure messages are written to ets
+      Logger.flush()
 
       {_ts, _level, msg, md} = log()
 
@@ -99,20 +110,20 @@ defmodule LoggerEtsBackendTest do
     end
 
     test "Allow `:all` to metadata" do
-      config metadata: []
-      Logger.debug "metadata", metadata1: "foo", metadata2: "bar"
+      config(metadata: [])
+      Logger.debug("metadata", metadata1: "foo", metadata2: "bar")
       Logger.flush()
       {_ts, _level, _msg, md} = log()
       assert md == []
 
-      config metadata: [:metadata3]
-      Logger.debug "metadata", metadata3: "foo", metadata4: "bar"
+      config(metadata: [:metadata3])
+      Logger.debug("metadata", metadata3: "foo", metadata4: "bar")
       Logger.flush()
       {_ts, _level, _msg, md} = log()
       assert md[:metadata3] == "foo"
 
-      config metadata: :all
-      Logger.debug "metadata", metadata5: "foo", metadata6: "bar"
+      config(metadata: :all)
+      Logger.debug("metadata", metadata5: "foo", metadata6: "bar")
       Logger.flush()
       {_ts, _level, _msg, md} = log()
       assert md[:metadata5] == "foo"
@@ -122,7 +133,8 @@ defmodule LoggerEtsBackendTest do
 
   defp config(opts) do
     Logger.configure_backend(@backend, opts)
-    Process.sleep(100)  # let the logging process catch-up
+    # let the logging process catch-up
+    Process.sleep(100)
   end
 
   defp ets_size() do
@@ -131,6 +143,7 @@ defmodule LoggerEtsBackendTest do
 
   defp log() do
     key = :ets.last(@name)
+
     case :ets.lookup(@name, key) do
       [val] -> val
       [] -> nil
